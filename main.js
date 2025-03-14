06cfa27805ec5c685e38b96d1e1e2d29b90efd33
Tuukka Heiskanen <tuukkaheiskanen5@gmail.com>
2025-03-14 20:04:34 +0200

    Kirjautumisvalikkoa paranneltu

--- main.js
+++ main.js
@@ -8,3 +8,31 @@
 
 
 // Kauppatavaran laittaminen näkyviin "hyllyyn":
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Dropdown valikko kirjautumiselle
+function kirjauduIkkuna() {
+    document.getElementById('id01').classList.toggle('hide')
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.kirjaududd')) {
+        let luokat = document.getElementById('id01').classList
+        if (luokat.contains('hide') === false) {
+            luokat.add('hide')
+        }
+    }
+}
