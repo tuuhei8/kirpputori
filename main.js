261d4b23f8aec2db61e300a30e89be975ea06833
Tuukka Heiskanen <tuukkaheiskanen5@gmail.com>
2025-03-15 15:52:17 +0200

    kirjautumisikkunan päivitys

--- main.js
+++ main.js
@@ -25,14 +25,15 @@
 
 // Dropdown valikko kirjautumiselle
 function kirjauduIkkuna() {
-    document.getElementById('id01').classList.toggle('hide')
+   // document.getElementById('id01').classList.toggle('hide')
+    document.getElementById('id01').classList.toggle('show')
 }
 
 window.onclick = function(event) {
     if (!event.target.matches('.kirjaududd')) {
         let luokat = document.getElementById('id01').classList
-        if (luokat.contains('hide') === false) {
-            luokat.add('hide')
+        if (luokat.contains('show')) {
+            luokat.remove('show')
         }
     }
 }
